cbc3a763f92bc2619199adc421546ef71a77d303
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2023-09-09 14:56:52 +0530

    notification stack imp, swipe to delete, tap to view in detail

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Notifications extends StatefulWidget {
-  const Notifications({super.key});
+  const Notifications({Key? key});
 
   @override
   State<Notifications> createState() => _NotificationsState();
 }
 
 class _NotificationsState extends State<Notifications> {
+  List<String> notifications = [
+    'Notification 1',
+    'Notification 2',
+    'Notification 3',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +22,59 @@ class _NotificationsState extends State<Notifications> {
         backgroundColor: Color(0xFFC4593B),
       ),
       backgroundColor: Colors.white,
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (BuildContext context, int index) {
+          final notification = notifications[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NotificationDetail(notification),
+                ),
+              );
+            },
+            child: Dismissible(
+              key: Key(notification), // Unique key for each notification
+              onDismissed: (direction) {
+                setState(() {
+                  notifications.removeAt(index);
+                });
+              },
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+              child: ListTile(
+                title: Text(notification),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NotificationDetail extends StatelessWidget {
+  final String notification;
+
+  NotificationDetail(this.notification);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification Detail'),
+        backgroundColor: Color(0xFF09DE35),
+      ),
       body: Center(
-        child: Text('Notifications'),
+        child: Text(notification),
       ),
     );
   }
