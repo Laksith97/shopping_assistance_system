8ae15a216ddc137883568a3ffb63c0e50fc836f8
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2023-09-13 04:08:29 +0530

    login fix where a user could enter without providing credentials

---
+++ lib/widgets/LoginPassword.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_assistance_system/constants.dart';
+import 'package:shopping_assistance_system/widgets/text_field_container.dart';
+
+class LoginPassword extends StatefulWidget {
+  final TextEditingController? controller; // Add this line
+
+  const LoginPassword({Key? key, this.controller})
+      : super(key: key); // Modify this line
+
+  @override
+  _RoundedPasswordFieldState createState() => _RoundedPasswordFieldState();
+}
+
+class _RoundedPasswordFieldState extends State<LoginPassword> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextFormField(
+        controller: widget.controller, // Add this line
+        obscureText: _obscureText,
+        cursorColor: kPrimaryColor,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your password';
+          }
+        },
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            color: kPrimaryColor,
+          ),
+          hintText: "Password",
+          hintStyle: TextStyle(fontFamily: 'OpenSans'),
+          suffixIcon: GestureDetector(
+            onTap: _togglePasswordVisibility,
+            child: Icon(
+              _obscureText ? Icons.visibility : Icons.visibility_off,
+              color: kPrimaryColor,
+            ),
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
